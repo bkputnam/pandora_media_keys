5097333c1bc92aa380481c94d3f2ef12f4d899c9
Teebs Teebson <teebs.teebson@gmail.com>
2022-02-23 11:44:14 -0700

    Fix nexttrack functionality so it works when only one SkipButton is present

--- extension/content-script.js
+++ extension/content-script.js
@@ -9,7 +9,23 @@ navigator.mediaSession.setActionHandler('previoustrack', function() {
 });
 
 navigator.mediaSession.setActionHandler('nexttrack', function() {
-  document.querySelector('.SkipButton[class*="SkipForward"]').click();
+  // Sometimes only the SkipForward button will be present and sometimes
+  // both SkipForward and SkipBackwards will be present. When it's just
+  // one button it will look like:
+  // button.SkipButton.Tuner__Control__Button.Tuner__Control__Skip__Button.TunerControl
+  // When there are two buttons they will look like:
+  // button.SkipButton.Tuner__Control__Button.Tuner__Control__SkipBack__Button.TunerControl
+  // button.SkipButton.Tuner__Control__Button.Tuner__Control__SkipForward__Button.TunerControl
+  //
+  // Classes observed on pandora.com on 2022-02-23
+  const skipKeys = document.querySelectorAll('.SkipButton');
+  if (skipKeys.length === 1) {
+    skipKeys[0].click();
+  } else {
+    [...skipKeys]
+        .find((el) => el.matches('[class*="SkipForward"]'))
+        .click();
+  }
 });
 
 navigator.mediaSession.setActionHandler('play', async function() {
